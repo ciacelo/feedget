d1eff6c15c5a2c458e68d5241633679dfa91ce99
ciacelo <ciacelojr@gmail.com>
2022-05-05 15:01:32 -0300

    add componentes utilizados na construção do Widget

---
+++ src/components/WidgetForm/ScreenshotButton.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { Camera, Trash } from 'phosphor-react'
+import html2canvas from 'html2canvas'
+import { Loading } from '../Loading'
+
+interface ScreenshotButtonProps {
+  screenshot: string | null,
+  onScreenshotTook: (base64: string | null) => void
+}
+
+export function ScreenshotButton({ screenshot, onScreenshotTook }: ScreenshotButtonProps) {
+  const [isTakingScreenShot, setIsTakingScreenShot] = useState(false)
+
+  async function handleTakeScreenshot() {
+    setIsTakingScreenShot(true)
+
+    const canvas = await html2canvas(document.querySelector('html')!)
+    const base64image = canvas.toDataURL('image/png')
+    onScreenshotTook(base64image)
+
+    setIsTakingScreenShot(false)
+  }
+
+  if (screenshot) {
+    return (
+      <button
+        type="button"
+        onClick={() => onScreenshotTook(null)}
+        className='p-1 w-10 h-10 flex justify-end items-end rounded-md text-zinc-400 relative hover:text-zinc-300 transition-colors'
+        style={{
+          backgroundImage: `url(${screenshot})`,
+        }}
+      >
+        <Trash weight='fill'/>
+      </button>
+    )
+  }
+
+  return (
+    <button
+      type="button"
+      onClick={handleTakeScreenshot}
+      className='p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors'
+    >
+      { isTakingScreenShot ? <Loading /> : <Camera className='w-6 h-6' />}
+    </button>
+  )
+}
